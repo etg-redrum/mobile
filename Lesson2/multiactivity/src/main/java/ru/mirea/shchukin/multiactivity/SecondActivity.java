8702a8fcf5bd2de320124579bacdb39c2d2cdb5b
Щукин Егор <shchukinegor1208@gmail.com>
2024-03-19 17:08:52 +0300

    Completed lesson 2

---
+++ Lesson2/multiactivity/src/main/java/ru/mirea/shchukin/multiactivity/SecondActivity.java
@@ -0,0 +1,93 @@
+package ru.mirea.shchukin.multiactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import ru.mirea.shchukin.multiactivity.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private AppBarConfiguration appBarConfiguration;
+    private ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        String text = (String) getIntent().getSerializableExtra("key");
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(text);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        setSupportActionBar(binding.toolbar);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_second);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAnchorView(R.id.fab)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_second);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+}
---
+++ Lesson2/toastapp/src/main/java/ru/mirea/shchukin/toastapp/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.mirea.shchukin.toastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CountSymbol(View view){
+        EditText symbol = findViewById(R.id.editTextText);
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "СТУДЕНТ № 29, ГРУППА БСБО-09-21 Количество символов - " + symbol.length(),
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
