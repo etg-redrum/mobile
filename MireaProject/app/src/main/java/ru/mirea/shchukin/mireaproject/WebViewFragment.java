52f31f1f0880757ebb45f6d4047ff4cff9fab589
shchukin <shchukinegor1208@gmail.com>
2024-03-29 16:54:05 +0300

    added MireaProject fragments

--- MireaProject/app/src/main/java/ru/mirea/shchukin/mireaproject/WebViewFragment.java
+++ MireaProject/app/src/main/java/ru/mirea/shchukin/mireaproject/WebViewFragment.java
@@ -7,16 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WebViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WebViewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -28,15 +24,6 @@ public class WebViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WebViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static WebViewFragment newInstance(String param1, String param2) {
         WebViewFragment fragment = new WebViewFragment();
         Bundle args = new Bundle();
@@ -58,7 +45,15 @@ public class WebViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        WebView webView = view.findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript support
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.google.com");
+        return view;
     }
 }
